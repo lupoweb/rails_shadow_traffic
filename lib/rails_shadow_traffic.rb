ef10e5c0fc84894c37f3bcd9133e1417fdf5b710
Francesco Lupano <lupano.web90@gmail.com>
2026-01-21 14:55:03 +0100

    feat: Scaffold gem structure with bundler

---
+++ lib/rails_shadow_traffic.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_shadow_traffic/version"
+
+module RailsShadowTraffic
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_shadow_traffic/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RailsShadowTraffic
+  VERSION = "0.1.0"
+end
